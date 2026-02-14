5c41d9cf759d017adf72cd0870a27e44df8df443
Saif Ahmed Nazmul <saifahmednazmul@gmail.com>
2026-02-15 00:58:52 +0600

    Remove sensitive password from db.js
    Removed the database password for security reasons.

--- practice-projects/05_chapter-login/db/db.js
+++ practice-projects/05_chapter-login/db/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host:"127.0.0.1",
   user:"root",
-  password:"saif@root",
+  password:"",
   database:"my_data",
   port:3306
 })
